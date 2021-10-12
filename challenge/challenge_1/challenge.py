a6fe50398d8bdc718ef2e60b3864d7529bb3a49a
Joe Lucas <joe@joetl.com>
2021-10-11 19:03:20 -0700

    prototype post request

--- challenge/challenge_1/challenge.py
+++ challenge/challenge_1/challenge.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+def classify(message):
+    return 'Spam'
+
+@app.route('/result', methods = ['POST'])
+def result():
+    if request.method == 'POST':
+        message = request.form.to_dict()
+        message = list(message.values())
+        result = classify(message)
+        return render_template("result.html", prediction = result)
+
 @app.route('/')
-def hello():
-    return 'Hello, World'
+def index():
+    return render_template('index.html')
