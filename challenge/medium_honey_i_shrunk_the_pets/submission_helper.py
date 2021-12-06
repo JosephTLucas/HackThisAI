dd17797a0abfb95e5cfee610880b7722bc67002d
Joseph Lucas <JosephTLucas@users.noreply.github.com>
2021-12-06 14:11:20 -0800

    Update submission_helper.py

--- challenge/medium_honey_i_shrunk_the_pets/submission_helper.py
+++ challenge/medium_honey_i_shrunk_the_pets/submission_helper.py
@@ -1,5 +1,5 @@
 import requests
-# Replace `your.csv` with a datafile of your choice.
-with open('setup/data/val/12500.jpg', 'rb') as f:
+# Replace `x.jpg` with a datafile of your choice.
+with open('x.jpg', 'rb') as f:
     r = requests.post('http://localhost:5000/check', files={'data_file': f})
     print(r.text)
