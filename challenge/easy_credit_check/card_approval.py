f5d0a8a3181961320315150c7a66794d26f824a2
Joe Lucas <joe@joetl.com>
2021-12-06 12:28:39 -0800

    working with flask

--- challenge/easy_credit_check/card_approval.py
+++ challenge/easy_credit_check/card_approval.py
@@ -1,20 +1,20 @@
 import csv
-from flask import Flask
+from flask import Flask, make_response, request
+import io
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
-app.run(debug=True, host='0.0.0.0')
+
 
 class predictor:
     def __init__(self):
         self.reg = LinearRegression()
         self.scaler = StandardScaler()
 
-    def train(self, input):
-        df = pd.read_csv(input)
+    def train(self, df):
         df[["Age", "Annual Income (k$)"]] = self.scaler.fit_transform(
             df[["Age", "Annual Income (k$)"]].values
         )
@@ -27,26 +27,30 @@ class predictor:
 
 
 def compare_data(input):
-    with open("Mall_Customers.csv", "r") as f:
-        standard = set(map(tuple, csv.reader(f)))
-    tampered = set(map(tuple, input))
-    return standard ^ tampered
+    standard = pd.read_csv("Mall_Customers.csv")
+    tampered = input
+    return len(pd.concat([standard, tampered]).drop_duplicates(keep=False))
+
 
 @app.route("/check", methods=["POST"])
 def check():
-    f = request.files['data_file']
+    f = request.files["data_file"]
     if not f:
-        return "No file"
+        response = "No file"
     stream = io.StringIO(f.stream.read().decode("UTF8"), newline=None)
-    csv_input = csv.reader(stream)
-    change = len(compare_data(csv_input))
+    df = pd.read_csv(stream)
+    change = compare_data(df)
     if change > 1:
-        return "Your tampering was too flagrant. You were discovered."
+        response = "Your tampering was too flagrant. You were discovered."
     else:
         p = predictor()
-        p.train(csv_input)
+        p.train(df)
         score = p.predict(p.scaler.transform(np.array([[19, 15]])))
         if score >= 90:
-            return "Enjoy your Platinum Mall Card: FLAG{HIGHROLLER}"
+            response = "Enjoy your Platinum Mall Card: FLAG{HIGHROLLER}"
         else:
-            return "You don't qualify. Try again."
+            response = "You don't qualify. Try again."
+    return make_response(response, 200)
+
+
+app.run(debug=True, host="0.0.0.0")
