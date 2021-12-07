e949e699a037937c416e7d1b90e386e768b9c0b0
Joe Lucas <joe@joetl.com>
2021-12-06 16:48:50 -0800

    flaskify rob vader

--- challenge/easy_rob_vader/README.md
+++ challenge/easy_rob_vader/README.md
@@ -4,12 +4,14 @@ In this challenge, you're trying to steal a sentiment analysis model.
 
 ## Instructions
 1. Run `docker build --tag rob_vader .`
-2. Run `docker run -dt --name rob_vader rob_vader:latest`
-3. Run `docker container exec -it rob_vader /bin/bash`
-    a. If you need to return to your terminal, use `exit`.
-4. Run `python steal.py "<your string>"`
+2. Run `docker run -it -p 5000:5000 --name rob_vader rob_vader:latest`
+3. Follow the instructions in `submission_helper.py` to understand the endpoints for querying the API and submitting your stolen model.
 
-When you have a working model that you want to submit:
-1. Create a `submission.py` file with a class called `Submit` with a method `get_score(str)->float`.
-2. In a separate window on your local filesystem, run: `docker cp submission.py rob_vader:/app`
-2. Back in your interactive docker terminal, run `python check.py`
+Reminder that you are going to submit a `.py` file that needs a structure like this:
+
+```python
+class Submit
+    def __init__(self)
+
+    def get_score(self, input: str) -> float:
+```
--- challenge/easy_rob_vader/steal.py
+++ challenge/easy_rob_vader/steal.py
@@ -1,8 +1,12 @@
 import nltk
+from flask import Flask, make_response, request
 
 nltk.download("vader_lexicon", quiet=True)
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import sys
+import importlib.util
+
+app = Flask(__name__)
 
 
 class Sentiment_analyzer:
@@ -12,7 +16,45 @@ class Sentiment_analyzer:
     def get_score(self, user_input):
         return self.sid.polarity_scores(user_input)["compound"]
 
+    def compare(self, model):
+        with open("corpus.txt", "r") as f:
+            corpus = f.readlines()
+            errors = 0
+            for text in corpus:
+                if round(self.get_score(text), 2) != round(model.get_score(text), 2):
+                    errors += 1
+        if errors > 0:
+            return (len(corpus) - errors) / errors
+        else:
+            return 1
 
-if __name__ == "__main__":
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.json
+    text = data["text"]
     s = Sentiment_analyzer()
-    print(s.get_score(sys.argv[1]))
+    return make_response(str(s.get_score(text)), 200)
+
+
+@app.route("/check", methods=["POST"])
+def check():
+    s = Sentiment_analyzer()
+    f = request.files["data_file"]
+    if not f:
+        response = "No file"
+    else:
+        f.save("submission.py")
+        spec = importlib.util.spec_from_file_location("module.name", "submission.py")
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        user_model = mod.Submit()
+        score = s.compare(user_model)
+        if score >= 0.95:
+            response = "Nice job, thief. FLAG{SNEAKYSNEAKY}"
+        else:
+            response = "Not Quite"
+    return make_response(response, 200)
+
+
+app.run(debug=True, host="0.0.0.0")
