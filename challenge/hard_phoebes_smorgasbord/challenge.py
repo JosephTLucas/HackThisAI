31c080c2e5e24f8b8c80267a4d862883c9a175e0
Joe Lucas <joe@joetl.com>
2021-12-20 18:44:52 -0800

    gunicorn

--- challenge/hard_phoebes_smorgasbord/challenge.py
+++ challenge/hard_phoebes_smorgasbord/challenge.py
@@ -68,5 +68,5 @@ def predict():
             response = [f"Probability of smorgasbord:\t{prob}", 200]
     return make_response(*response)
 
-
-app.run(debug=True, host="0.0.0.0")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
