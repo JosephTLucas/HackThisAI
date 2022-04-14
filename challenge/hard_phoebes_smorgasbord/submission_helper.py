49add2fb336e06871725c772db7c73872c822057
Joe Lucas <joe@joetl.com>
2022-04-14 14:19:32 -0700

    Fix Issue #16

--- challenge/hard_phoebes_smorgasbord/submission_helper.py
+++ challenge/hard_phoebes_smorgasbord/submission_helper.py
@@ -4,7 +4,9 @@ import requests
 with open("example.csv", "rb") as f:
     r = requests.post("http://localhost:5000/predict", files={"data_file": f})
     print(r.text)
+'''
 # To get a submit something
 with open("example.csv", "rb") as f:
     r = requests.post("http://localhost:5000/check", files={"data_file": f})
     print(r.text)
+'''
